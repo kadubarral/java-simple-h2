0ea935480b824c5f5f9453feca15faed65aae0c7
kadubarral <kadubarral@gmail.com>
2020-11-28 17:25:35 +0000

    branch with issues

--- src/main/java/io/barral/JavaH2.java
+++ src/main/java/io/barral/JavaH2.java
@@ -1,105 +1,98 @@
-package io.barral;
+package feup.tvvs;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.Date;
-import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.h2.tools.DeleteDbFiles;
-import static java.sql.DriverManager.*;
 
-public class JavaH2 {
+public class staticanalisys {
 
-    private static final Logger LOGGER = Logger.getLogger(JavaH2.class.getName());
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws SQLException, UnknownHostException, InterruptedException {
+        try {
 
-        // delete the database named 'test' in the user home directory
-        DeleteDbFiles.execute("~", "test", true);
+            // delete the database named 'test' in the user home directory
+            DeleteDbFiles.execute("~", "test", true);
 
-        String user = "user";
-        char[] password = {'t', 'i', 'a', 'E', 'T', 'r', 'p'};
-        String servername = InetAddress.getLocalHost().getHostName();
+            Class.forName("org.h2.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:h2:~/test");
+            Statement stat = conn.createStatement();
 
-        Properties prop = new Properties();
-        prop.setProperty("user", user);
-        prop.put("password", password);
+            String servername = InetAddress.getLocalHost().getHostName();
 
-        try (Connection conn = getConnection("jdbc:h2:~/test", prop)) {
-            try (Statement stat = conn.createStatement()) {
+            //create table
+            stat.execute("CREATE TABLE ACTIVITY (ID INTEGER, STARTTIME datetime, ENDTIME datetime, SERVERNAME VARCHAR(200), ACTIVITYNAME VARCHAR(200), PRIMARY KEY (ID))");
 
-                //create table
-                stat.execute("CREATE TABLE ACTIVITY (ID INTEGER, STARTTIME datetime, ENDTIME datetime, SERVERNAME VARCHAR(200), ACTIVITYNAME VARCHAR(200), PRIMARY KEY (ID))");
+            //prepared statement
+            PreparedStatement prep = conn.prepareStatement("INSERT INTO ACTIVITY (ID, STARTTIME, ENDTIME, SERVERNAME, ACTIVITYNAME) VALUES (?,?,?,?,?)");
 
-                //prepared statement
-                try (PreparedStatement prep = conn.prepareStatement("INSERT INTO ACTIVITY (ID, STARTTIME, ENDTIME, SERVERNAME, ACTIVITYNAME) VALUES (?,?,?,?,?)")) {
+            //insert 10 row data
+            for (int i = 0; i<10; i++){
+                prep.setLong(1, i);
+                prep.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
+                Thread.sleep(500);
+                prep.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+                prep.setString(4, servername);
+                prep.setString(5, "Activity-" + i);
 
-                    //insert 10 row data
-                    for (int i = 0; i < 10; i++) {
-                        prep.setLong(1, i);
-                        prep.setTimestamp(2, new Timestamp(System.currentTimeMillis()));
-                        Thread.sleep(500);
-                        prep.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
-                        prep.setString(4, servername);
-                        prep.setString(5, "Activity-" + i);
-
-                        //batch insert
-                        prep.addBatch();
-                    }
-                    conn.setAutoCommit(false);
-                    prep.executeBatch();
-                }
-                conn.setAutoCommit(true);
-
-                ResultSet rst;
-                rst = stat.executeQuery("Select count(*) from ACTIVITY");
-                double total;
-                if (rst.next()) {
-                    total = rst.getDouble(1);
-                } else {
-                    total = 0.0;
-                }
-                Integer count = 1;
-
-                //query to database
-                try {
-
-                    ResultSet rs;
-                    try (PreparedStatement prep2 = conn.prepareStatement("Select STARTTIME, ENDTIME, ACTIVITYNAME, SERVERNAME from ACTIVITY where SERVERNAME = ?")) {
-                        prep2.setString(1, servername);
-                        rs = prep2.executeQuery();
-
-                        while (rs.next()) {
-
-                            Date start = rs.getTimestamp(1);
-                            Date end = rs.getTimestamp(2);
-                            String activityName = rs.getString(3);
-                            String serverName = rs.getString(4);
-
-                            //print query result to console
-                            LOGGER.log(Level.INFO,"activity: {0} ", activityName);
-                            LOGGER.log(Level.INFO,"local: {0} ", serverName);
-                            LOGGER.log(Level.INFO,"start: {0} ", start);
-                            LOGGER.log(Level.INFO,"end: {0} ", end);
-                            if (total == 0) {
-                                throw new UnsupportedOperationException("Can't divide by zero!");
-                            } else {
-                                LOGGER.log(Level.INFO,"% done: {0} ", (count / total) * 100);
-                            }
-                            count++;
-                        }
-                    }
-                } catch (SQLException | UnsupportedOperationException e) {
-                    LOGGER.log(Level.SEVERE, "context", e);
-                }
+                //batch insert
+                prep.addBatch();
             }
+            conn.setAutoCommit(false);
+            prep.executeBatch();
+            conn.setAutoCommit(true);
+
+            double num1=0.0, num2=0.0;
+            double sum=0.0, sub=0.0, multiple=0.0;
+            double divide=0.0, remainder=0.0, power=0.0;
+            char operator='\0';
+            boolean nextOperation = true;
+            char ch='\0';
+
+            ResultSet rst = stat.executeQuery("Select count(*) from ACTIVITY");
+            Double total = 0.0;
+            if (rst.next())
+            {
+                total = rst.getDouble(1);
+            }
+
+            Integer count = 1;
+
+            //query to database
+            try {
+                ResultSet rs = stat.executeQuery("Select STARTTIME, ENDTIME, ACTIVITYNAME, SERVERNAME from ACTIVITY where SERVERNAME = '" + servername + "'");
+                while (rs.next()) {
+
+                    Date start = rs.getTimestamp(1);
+                    Date end = rs.getTimestamp(2);
+                    String activityName = rs.getString(3);
+                    String serverName = rs.getString(4);
+
+                    //print query result to console
+                    System.out.println("activity: " + activityName);
+                    System.out.println("local: " + serverName);
+                    System.out.println("start: " + start);
+                    System.out.println("end: " + end);
+                    System.out.println("% done: " + (count/total)*100);
+                    System.out.println("--------------------------");
+
+                    count++;
+                }
+                rs.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            //close connection
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 }
